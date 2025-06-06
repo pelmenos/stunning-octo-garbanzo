a9aec19c92c914f8f73d59b4bef9588d5f540c22
pelmenos <vadimshvynda@gmail.com>
2025-06-06 14:59:17 +0700

    Каркас проекта

---
+++ main.py
@@ -0,0 +1,21 @@
+def get_user_input():
+    pass
+
+
+def show_info():
+    pass
+
+
+def processed_result(result):
+    pass
+
+
+def save_result(result):
+    pass
+
+
+if __name__ == '__main__':
+    show_info()
+    result = get_user_input()
+    processed_result(result)
+    save_result(result)
